a331d36da9a0bdbe6b91f878550706460958f547
Anjana Vakil <contact@anjana.dev>
2024-03-18 13:43:59 -0700

    Basic TODO app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,87 @@
 import automergeLogo from '/automerge.png'
 import './App.css'
-import { default as A } from "@automerge/automerge/next"
-import { AutomergeUrl } from '@automerge/automerge-repo'
-import { useDocument } from '@automerge/automerge-repo-react-hooks'
+import { isValidAutomergeUrl, type AutomergeUrl, } from '@automerge/automerge-repo'
+import { useDocument, useRepo } from '@automerge/automerge-repo-react-hooks'
 
-interface CounterDoc {
-  counter: A.Counter
+
+interface TodoDoc {
+  task: string;
+  done: boolean;
 }
 
-function App({ docUrl }: { docUrl: AutomergeUrl }) {
-  const [doc, changeDoc] = useDocument<CounterDoc>(docUrl)
+interface ListDoc {
+  todoUrls: AutomergeUrl[];
+}
+
+
+function TodoItem({ docUrl }: { docUrl: AutomergeUrl }) {
+
+  const [doc, changeDoc] = useDocument<TodoDoc>(docUrl)
+
+  return (<div className="task" >
+
+    {doc ? (<>
+      <input
+        type="checkbox"
+        checked={doc.done === undefined ? false : doc.done}
+        onChange={() => changeDoc(d => d.done = !d.done)}
+        style={{ textDecoration: 'line-through' }} />
+
+      <input type="text" placeholder='Learn Automerge' value={doc.task || ''} onChange={(e) => changeDoc(d => d.task = e.target.value)} />
+    </>
+    ) : (
+      <p>Loading...</p>
+    )}
+
+  </div>
+  )
+};
+
+
+function App({ }) {
+  const repo = useRepo();
+
+  const rootDocUrl = `${document.location.hash.substring(1)}`
+  let handle
+  if (isValidAutomergeUrl(rootDocUrl)) {
+    handle = repo.find(rootDocUrl)
+  } else {
+    handle = repo.create<ListDoc>()
+    handle.change(d => d.todoUrls = [])
+  }
+  const docUrl = document.location.hash = handle.url
+
+  const [listDoc, changeListDoc] = useDocument<ListDoc>(docUrl)
+
+
+  console.log(listDoc);
 
   return (
     <>
-      <div>
-        <a href="https://automerge.org" target="_blank">
-          <img src={automergeLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Meet Automerge</h1>
-      <div className="card">
-        <button onClick={() => changeDoc((d) => d.counter.increment(1))}>
-          count is {doc && doc.counter.value}
-        </button>
-        <p>
-          Open this page in another tab to watch the updates synchronize
+      <header>
+        <h1>Shared Task List</h1>
+      </header>
+
+
+      <button type="button" onClick={() => {
+        const newTodo = repo.create<TodoDoc>();
+        changeListDoc(d => d.todoUrls = d.todoUrls
+          ? [newTodo.url, ...d.todoUrls,]
+          : [newTodo.url]
+        );
+      }}>
+        New todo item
+      </button>
+
+      {listDoc && listDoc.todoUrls?.map(docUrl =>
+        <TodoItem key={docUrl} docUrl={docUrl} />)}
+
+
+
+      <footer>
+        <p>Powered by Automerge and Vite + React + TypeScript
         </p>
-      </div>
-      <p className="read-the-docs">
-        Built with Automerge, Vite, React, and TypeScript
-      </p>
+      </footer>
     </>
   )
 }
