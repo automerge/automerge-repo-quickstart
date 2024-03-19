cf3081f272d892ba2e5b77d3ae526270e6f87a61
Peter van Hardenberg <pvh@pvh.ca>
2024-03-18 23:32:58 -0700

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,17 @@ import react from "@vitejs/plugin-react"
 import wasm from "vite-plugin-wasm"
 
 export default defineConfig({
+  // customize this to your repo name for github pages deploy
+  base: "/",
+
+  build: {
+    target: "esnext",
+  },
+
   plugins: [wasm(), react()],
 
   worker: {
     format: "es",
     plugins: () => [wasm()],
-  },
-
-  optimizeDeps: {
-    // This is necessary because otherwise `vite dev` includes two separate
-    // versions of the JS wrapper. This causes problems because the JS
-    // wrapper has a module level variable to track JS side heap
-    // allocations, and initializing this twice causes horrible breakage
-    exclude: [
-      "@automerge/automerge-wasm",
-      "@automerge/automerge-wasm/bundler/bindgen_bg.wasm",
-      "@syntect/wasm",
-    ],
   },
 })
