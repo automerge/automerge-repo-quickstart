41a379e8a7a6d4c0a8e8f9340f40fc39da1dfddb
Peter van Hardenberg <pvh@pvh.ca>
2025-05-08 00:17:02 -0700

    remove automerge

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,78 @@
+import "@picocss/pico/css/pico.min.css";
+import "../App.css";
+import { useState } from "react";
+
+export interface Task {
+  title: string;
+  done: boolean;
+}
+
+export interface TaskList {
+  title: string;
+  tasks: Task[];
+}
+
+// A helper function to consistently initialize a task list.
+export function initTaskList(): TaskList {
+  return {
+    title: `TODO: ${new Date().toLocaleString()}`,
+    tasks: [{ done: false, title: "" }],
+  };
+}
+
+export const TaskList: React.FC<{
+  taskList: TaskList;
+}> = ({ taskList }) => {
+  const [tasks, setTasks] = useState(taskList.tasks);
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          setTasks([
+            ...tasks,
+            {
+              title: "",
+              done: false,
+            },
+          ]);
+        }}
+      >
+        <b>+</b> New task
+      </button>
+
+      <div id="task-list">
+        {tasks?.map(({ title, done }, index) => (
+          <div className="task" key={index}>
+            <input
+              type="checkbox"
+              checked={done}
+              onChange={() =>
+                setTasks(
+                  tasks.map((task, i) =>
+                    i === index ? { ...task, done: !task.done } : task,
+                  ),
+                )
+              }
+            />
+
+            <input
+              type="text"
+              placeholder="What needs doing?"
+              value={title || ""}
+              onChange={(e) =>
+                setTasks(
+                  tasks.map((task, i) =>
+                    i === index ? { ...task, title: e.target.value } : task,
+                  ),
+                )
+              }
+              style={done ? { textDecoration: "line-through" } : {}}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App, { type TaskList } from './App.tsx'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.tsx";
+import "@picocss/pico/css/pico.min.css";
+import "./index.css";
 
-import './index.css'
+import { initTaskList } from "./components/TaskList.tsx";
 
-import {
-  isValidAutomergeUrl,
-  Repo,
-  WebSocketClientAdapter,
-  IndexedDBStorageAdapter,
-  RepoContext
-} from '@automerge/react'
-
-const repo = new Repo({
-  network: [new WebSocketClientAdapter("wss://sync.automerge.org")],
-  storage: new IndexedDBStorageAdapter(),
-})
-
-
-const rootDocUrl = `${document.location.hash.substring(1)}`
-let handle
-if (isValidAutomergeUrl(rootDocUrl)) {
-  handle = await repo.find(rootDocUrl)
-} else {
-  handle = repo.create<TaskList>({tasks: []})
-}
-const docUrl = document.location.hash = handle.url
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RepoContext.Provider value={repo}>
-      <App docUrl={docUrl} />
-    </RepoContext.Provider>
+    <Suspense fallback={<div>Loading a document...</div>}>
+      <App taskList={initTaskList()} />
+    </Suspense>
   </React.StrictMode>,
-)
+);
