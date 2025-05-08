2911a72862a681c173b363a43016fa5429d1b19b
Peter van Hardenberg <pvh@pvh.ca>
2025-05-08 00:17:02 -0700

    remove automerge

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import automergeLogo from "/automerge.png";
 import "@picocss/pico/css/pico.min.css";
 import "./App.css";
-import { useDocument, updateText, type AutomergeUrl } from "@automerge/react";
+import { useState } from "react";
 
 export interface Task {
   title: string;
@@ -12,8 +12,8 @@ export interface TaskList {
   tasks: Task[];
 }
 
-function App({ docUrl }: { docUrl: AutomergeUrl }) {
-  const [doc, changeDoc] = useDocument<TaskList>(docUrl);
+function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   return (
     <>
@@ -27,46 +27,48 @@ function App({ docUrl }: { docUrl: AutomergeUrl }) {
       <button
         type="button"
         onClick={() => {
-          changeDoc((d) =>
-            d.tasks.unshift({
+          setTasks([
+            ...tasks,
+            {
               title: "",
               done: false,
-            })
-          );
+            },
+          ]);
         }}
       >
         <b>+</b> New task
       </button>
 
       <div id="task-list">
-        {doc &&
-          doc.tasks?.map(({ title, done }, index) => (
-            <div className="task" key={index}>
-              <input
-                type="checkbox"
-                checked={done}
-                onChange={() =>
-                  changeDoc((d) => {
-                    d.tasks[index].done = !d.tasks[index].done;
-                  })
-                }
-              />
+        {tasks?.map(({ title, done }, index) => (
+          <div className="task" key={index}>
+            <input
+              type="checkbox"
+              checked={done}
+              onChange={() =>
+                setTasks(
+                  tasks.map((task, i) =>
+                    i === index ? { ...task, done: !task.done } : task
+                  )
+                )
+              }
+            />
 
-              <input
-                type="text"
-                placeholder="What needs doing?"
-                value={title || ""}
-                onChange={(e) =>
-                  changeDoc((d) => {
-                    // Use Automerge's updateText for efficient multiplayer edits
-                    // (as opposed to replacing the whole title on each edit)
-                    updateText(d.tasks[index], ["title"], e.target.value);
-                  })
-                }
-                style={done ? { textDecoration: "line-through" } : {}}
-              />
-            </div>
-          ))}
+            <input
+              type="text"
+              placeholder="What needs doing?"
+              value={title || ""}
+              onChange={(e) =>
+                setTasks(
+                  tasks.map((task, i) =>
+                    i === index ? { ...task, title: e.target.value } : task
+                  )
+                )
+              }
+              style={done ? { textDecoration: "line-through" } : {}}
+            />
+          </div>
+        ))}
       </div>
 
       <footer>
--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App, { type TaskList } from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-import './index.css'
+import "./index.css";
 
-import {
-  isValidAutomergeUrl,
-  Repo,
-  WebSocketClientAdapter,
-  IndexedDBStorageAdapter,
-  RepoContext
-} from '@automerge/react'
-
-const repo = new Repo({
-  network: [new WebSocketClientAdapter("wss://sync.automerge.org")],
-  storage: new IndexedDBStorageAdapter(),
-})
-
-
-const rootDocUrl = `${document.location.hash.substring(1)}`
-let handle
-if (isValidAutomergeUrl(rootDocUrl)) {
-  handle = await repo.find(rootDocUrl)
-} else {
-  handle = repo.create<TaskList>({tasks: []})
-}
-const docUrl = document.location.hash = handle.url
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RepoContext.Provider value={repo}>
-      <App docUrl={docUrl} />
-    </RepoContext.Provider>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
