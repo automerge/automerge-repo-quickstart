1e36fa43f4a0a7bd9a58bf5ba4b0a6c0e046d872
Peter van Hardenberg <pvh@pvh.ca>
2025-05-22 09:37:24 +0200

    Add Repo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
-import "@picocss/pico/css/pico.min.css";
 import "./index.css";
 
 import { initTaskList } from "./components/TaskList.tsx";
+import {
+  Repo,
+  BroadcastChannelNetworkAdapter,
+  IndexedDBStorageAdapter,
+} from "@automerge/react";
+
+const repo = new Repo({
+  network: [new BroadcastChannelNetworkAdapter()],
+  storage: new IndexedDBStorageAdapter(),
+});
+
+// Add the repo to the global window object so it can be accessed in the browser console
+// This is useful for debugging and testing purposes.
+declare global {
+  interface Window {
+    repo: Repo;
+  }
+}
+window.repo = repo;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
