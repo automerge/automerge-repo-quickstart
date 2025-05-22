ba1d44396b66ad0d466903aefa7288a6c25d0e38
Peter van Hardenberg <pvh@pvh.ca>
2025-05-22 09:37:24 +0200

    Load the document from the location hash

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import "./index.css";
 
-import { initTaskList } from "./components/TaskList.tsx";
+import { initTaskList, TaskList } from "./components/TaskList.tsx";
 import {
   Repo,
   BroadcastChannelNetworkAdapter,
   IndexedDBStorageAdapter,
+  isValidAutomergeUrl,
+  DocHandle,
 } from "@automerge/react";
 
 const repo = new Repo({
@@ -20,9 +22,22 @@ const repo = new Repo({
 declare global {
   interface Window {
     repo: Repo;
+    // We also add the handle to the global window object for debugging
+    handle: DocHandle<TaskList>;
   }
 }
 window.repo = repo;
+
+// Check the URL for a document to load
+const locationHash = document.location.hash.substring(1);
+// Depending if we have an AutomergeUrl, either find or create the document
+if (isValidAutomergeUrl(locationHash)) {
+  window.handle = await repo.find(locationHash);
+} else {
+  window.handle = repo.create<TaskList>(initTaskList());
+  // Set the location hash to the new document we just made.
+  document.location.hash = window.handle.url;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
